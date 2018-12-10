3e1bb55eaabd695335bdfb7bac5946b33aacdaea
Andrew Adhigunarsa <adhigunarsa@Andrews-MacBook-Pro.local>
2018-12-11 05:13:13 +1100

    feat: submit more changes

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavbarHeader from './navbar/NavbarHeader';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavbarHeader />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/header/navbar/NavbarHeader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NavbarHeader extends React.Component {
+  render() {
+    return (
+      <ul>
+        <li>Test</li>
+      </ul>
+    );
+  }
+}
+
+export default NavbarHeader;
