29d138673dd965dafec7c854ea83f52519272f16
Andrew Adhigunarsa <adhigunarsa@192-168-1-162.tpgi.com.au>
2019-07-06 00:38:52 +1000

    test

---
+++ src/pages/component-preview/ComponentPreview.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ComponentPreview extends Component {
+  render() {
+    return <div className="component-preview">hello</div>;
+  }
+}
+
+export default ComponentPreview;
