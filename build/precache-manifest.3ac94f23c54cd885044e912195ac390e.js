33231cf78a01e6713537f80456ad0500e4fea20e
Andrew Adhigunarsa <andrewadhigunarsa@192-168-1-107.tpgi.com.au>
2019-09-08 01:51:52 +1000

    test

---
+++ build/precache-manifest.3ac94f23c54cd885044e912195ac390e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "e2e907ff30c554360310",
+    "url": "/react-portfolio/static/js/main.bb4764ac.chunk.js"
+  },
+  {
+    "revision": "355b2458857d03c5fc4e",
+    "url": "/react-portfolio/static/js/2.fd9a51cf.chunk.js"
+  },
+  {
+    "revision": "e2e907ff30c554360310",
+    "url": "/react-portfolio/static/css/main.b0ce8e2b.chunk.css"
+  },
+  {
+    "revision": "bb4c5cf8ff6c6389448ffff05f4cad97",
+    "url": "/react-portfolio/index.html"
+  }
+];
