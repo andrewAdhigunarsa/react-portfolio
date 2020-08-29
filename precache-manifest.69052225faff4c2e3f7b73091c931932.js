74419ba7afbb7d5527f194c8d52e76a8fe89f1d1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-29 14:02:01 +0000

    Deploy andrewAdhigunarsa/react-portfolio to github.com/andrewAdhigunarsa/react-portfolio.git:gh-pages

---
+++ precache-manifest.69052225faff4c2e3f7b73091c931932.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f3ac5a3c5e11a32ed66f",
+    "url": "/static/js/main.07e5579e.chunk.js"
+  },
+  {
+    "revision": "efec105ee8c8dbcf6590",
+    "url": "/static/js/2.32484a56.chunk.js"
+  },
+  {
+    "revision": "f3ac5a3c5e11a32ed66f",
+    "url": "/static/css/main.6028ecab.chunk.css"
+  },
+  {
+    "revision": "2feeaf8a1072ba6ea1efc03b2957df3c",
+    "url": "/index.html"
+  }
+];
